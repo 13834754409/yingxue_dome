6e017a39bd9cfd3701762eedd8b2b9a105233923
lih <qszy323@163.com>
2020-12-01 18:23:28 +0800

    第一次提交项目

---
+++ src/main/java/com/lih/YingxueDomeApplication.java
@@ -0,0 +1,13 @@
+package com.lih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YingxueDomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YingxueDomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lih/YingxueDomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YingxueDomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
