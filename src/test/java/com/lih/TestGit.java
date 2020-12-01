5b850905d66f0cb31f7e918170293696a12e60e6
lih <qszy323@163.com>
2020-12-01 18:27:49 +0800

    测试类中添加一个方法

--- src/test/java/com/lih/TestGit.java
+++ src/test/java/com/lih/TestGit.java
@@ -1,9 +1,18 @@
 package com.lih;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 /**
  * @author:lih
  * @Description:
  * @Date:2020/12/01 18:24
  */
+@SpringBootTest
 public class TestGit {
+    @Test
+    public void test0(){
+        int a = 100;
+        System.out.println(a);
+    }
 }
