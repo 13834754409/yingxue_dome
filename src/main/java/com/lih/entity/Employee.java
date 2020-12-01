486a8aad92d1795f249252935c8c6afa2287bd19
yanww <yanww0316@163.com>
2020-12-01 20:25:09 +0800

    yanww:第三次修改提交

--- src/main/java/com/lih/entity/Employee.java
+++ src/main/java/com/lih/entity/Employee.java
@@ -10,4 +10,36 @@ public class Employee {
    private String name;
    private Character sex;
    private Double salary;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Character getSex() {
+        return sex;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
