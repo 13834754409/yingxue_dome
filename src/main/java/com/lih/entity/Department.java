33c18123d82c5013546af526978a9bae72c7d991
yanww <yanww0316@163.com>
2020-12-01 20:22:23 +0800

    yanww:第二次修改提交

---
+++ src/main/java/com/lih/entity/Department.java
@@ -0,0 +1,12 @@
+package com.lih.entity;
+
+/**
+ * @author yww
+ * @Description
+ * @Date 2020/12/01 20:21
+ */
+public class Department {
+    private String id;
+    private String name;
+    private Integer departmentCount;
+}
---
+++ src/main/java/com/lih/entity/Employee.java
@@ -0,0 +1,13 @@
+package com.lih.entity;
+
+/**
+ * @author yww
+ * @Description
+ * @Date 2020/12/01 20:20
+ */
+public class Employee {
+   private String id;
+   private String name;
+   private Character sex;
+   private Double salary;
+}
